86a962c41799b5262f5b73c49a1157138d70b4a0
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-08-27 16:54:26 +0300

    Task2. pending

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,76 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-// import Notiflix from 'notiflix';
+import flatpickr from 'flatpickr';
+import '/node_modules/flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+import '../css/common.css';
 
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+};
+refs.startBtn.setAttribute('disabled', '');
+let timerId = null;
+const date = new Date();
+let delta = 0;
+refs.startBtn.addEventListener('click', runTimer);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  allowInput: true,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < date) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+    } else {
+      refs.startBtn.removeAttribute('disabled', '');
+      console.log('else', selectedDates[0]);
+
+      delta = selectedDates[0].getTime() - date;
+    }
+  },
+};
+
+flatpickr(refs.input, options);
+require('flatpickr/dist/themes/confetti.css');
+
+function runTimer() {
+  console.log(delta);
+}
+
+const timer = {
+  start() {
+    setInterval(() => {
+      renderTimer(delta);
+    }, 1000);
+  },
+};
+
+timer.start();
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 // const refs = {
 //   input: document.querySelector('#datetime-picker'),
 //   btnStart: document.querySelector('[data-start]'),
