6187ab48c866d1e81ecd29a9772e165c6f64ab66
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-09-04 20:57:28 +0300

    HW09 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,34 @@
 import Notiflix from 'notiflix';
 
 const promiseForm = document.querySelector('.form');
-
 promiseForm.addEventListener('submit', onClickBtn);
-  function onClickBtn(e) {
+
+function onClickBtn(e) {
     e.preventDefault();
-    const form = e.currentTarget;
-    const amount = form.elements.amount.value
-    
-    const firstDelay = form.elements.delay.value;
-    const delayStep = form.elements.step.value;
-    for (let i = 0; i <= amount; i++) {
-    function createPromise(position, delay) {
-  
-    return new Promise((resolve, reject) => {   
-        
+    const form = e.currentTarget; 
+    const amount = Number(form.elements.amount.value);
+    let firstDelay = Number(form.elements.delay.value);
+    const delayStep = Number(form.elements.step.value);
+    for (let counter = 1; counter <= amount; counter +=1) {
+      
+    function createPromise(counter, delayStep) {
+      return new Promise((resolve, reject) => {   
       const shouldResolve = Math.random() > 0.3;
       setTimeout(() => {
         if (shouldResolve) {
-        resolve({ position:`${i}`, delay:`${delayStep}` });
-        }
-        reject({ position:`${i}`, delay:`${delayStep}` });
-            }, firstDelay);
+        resolve({ counter, delayStep });
+  }
+      reject({ counter, delayStep });
+      }, firstDelay);
    });
-  }    
-  }
-    
-    createPromise(amount, `${delayStep}`)
-  .then(({ position, delay }) => {
-    
-    Notiflix.Notify.warning(`✅ Fulfilled promise ${position} in ${delay}ms`);
+     }   
+  createPromise(counter, delayStep)
+  .then(({ counter, delayStep }) => {
+    Notiflix.Notify.info(`✅ Fulfilled promise ${counter} in ${delayStep}ms`);
   })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-  })
-  // .finally(() => form.reset());
+  .catch(({ counter, delayStep }) => {
+    Notiflix.Notify.warning(`❌ Rejected promise ${counter} in ${delayStep}ms`);
+  }).finally(() => form.reset());
+  firstDelay += delayStep;
   }
-
-  
+}
